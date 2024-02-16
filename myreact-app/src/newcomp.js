0cc65ec4bff4656105fe55b6d6f36bb86219d171
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-16 11:18:58 +0600

    Props

--- myreact-app/src/newcomp.js
+++ myreact-app/src/newcomp.js
@@ -1,8 +1,11 @@
-function Nc() {
+import React from "react";
+
+
+function Nc(props) {
   return (
   <div>
-  <h1>Farhan Labib</h1>
-  <h2>hello world</h2>
+  <h1>Whats your name?</h1>
+  <h2>Hello Im {props.names}</h2>
   </div>
   );
 }
