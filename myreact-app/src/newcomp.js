a81aba8f3eab2e216a4d28a614733e4a2567f4be
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-16 15:12:44 +0600

    arrow function

--- myreact-app/src/newcomp.js
+++ myreact-app/src/newcomp.js
@@ -1,16 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Nc(props) {
+const Nc = (props) => {
   return (
   <div>
   <h1>Whats your name?</h1>
   <h2>Hello Im {props.names}</h2>
   <h2>Age: {props.age}</h2>
-
   </div>
   );
-}
+};
 
 Nc.propTypes={
   names: PropTypes.string,
