96fbea44128fe46d386c63a6b378215eac2c48ab
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-16 15:35:28 +0600

    class component

--- myreact-app/src/index.js
+++ myreact-app/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Nc,App2} from './newcomp';
+import Newcomp from './newcomp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nc age={24}/>
-    <App2/>
+    <Newcomp/>
   </React.StrictMode>
 );
 
--- myreact-app/src/newcomp.js
+++ myreact-app/src/newcomp.js
@@ -1,29 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const Nc = (props) => {
-  return (
-  <div>
-  <h1>Whats your name?</h1>
-  <h2>Hello Im {props.names}</h2>
-  <h2>Age: {props.age}</h2>
-  <App2/>
-  </div>
+
+
+class Newcomp extends React.Component {
+  render(){
+    return(
+      <h1>MD. FARHAN LABIB JAHIN</h1>
+    );
+  };
+}
+export default Newcomp;
+// export const Nc = (props) => {
+//   return (
+//   <div>
+//   <h1>Whats your name?</h1>
+//   <h2>Hello Im {props.names}</h2>
+//   <h2>Age: {props.age}</h2>
+//   <App2/>
+//   </div>
   
-  );
-};
+//   );
+// };
 
-export const App2=()=><h1>react app</h1>;
+// export const App2=()=><h1>react app</h1>;
 
-Nc.propTypes={
-  names: PropTypes.string,
-  age:PropTypes.number,
+// Nc.propTypes={
+//   names: PropTypes.string,
+//   age:PropTypes.number,
   
-};
+// };
 
-Nc.defaultProps={
-  names: "jahin",
-  age: 27
-};
+// Nc.defaultProps={
+//   names: "jahin",
+//   age: 27
+// };
 
 
