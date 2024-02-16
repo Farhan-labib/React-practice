b1fff2520388a6ed83b4c740a4c197b9b3f6d1aa
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-16 12:04:53 +0600

    proptypes implementation

--- myreact-app/src/newcomp.js
+++ myreact-app/src/newcomp.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 
 function Nc(props) {
   return (
@@ -7,9 +7,15 @@ function Nc(props) {
   <h1>Whats your name?</h1>
   <h2>Hello Im {props.names}</h2>
   <h2>Age: {props.age}</h2>
-  {props.children}
+
   </div>
   );
 }
+
+Nc.propTypes={
+  names: PropTypes.string,
+  age:PropTypes.number
+};
+
 
 export default Nc;
