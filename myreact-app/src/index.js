42a25514e573aa1ce36ce335a4c58ce7b8290dc9
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-16 11:00:13 +0600

    making components

--- myreact-app/src/index.js
+++ myreact-app/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Nc from './newcomp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nc/>
+    <App/>
   </React.StrictMode>
 );
 
---
+++ myreact-app/src/newcomp.js
@@ -0,0 +1,10 @@
+function Nc() {
+  return (
+  <div>
+  <h1>Farhan Labib</h1>
+  <h2>hello world</h2>
+  </div>
+  );
+}
+
+export default Nc;
