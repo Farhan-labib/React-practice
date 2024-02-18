88737ead9da607b771d8239a317c50d599509fa1
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-19 01:09:39 +0600

    multiple infut with usestate

--- myreact-app/src/USComponent.js
+++ myreact-app/src/USComponent.js
@@ -2,14 +2,33 @@ import React,{useState, useEffect} from 'react';
 
 export const USComponent=()=>{
 
+    const [info,setData]=useState({
+        name:"",
+        email:""
+    });
+    console.log(info)
+  const  changedata=(e)=>{
+        const name=e.target.name;
+        const email=e.target.email;
 
-    const [info,stinfo]=useState("");
-    console.log(info);
+        const value=e.target.value;
 
+    
+    setData((prev)=>{
+        return{
+            ...prev, [name]:value
+
+        }
+
+    })}
+    
     return (
 <div>
 <form>
-<input type='text' value={info} onChange={(e)=>stinfo(e.target.value)}/>
+<input type='text' name='name' value={info.name} onChange={changedata}/>
+<br></br>
+<input type='text' name="email" value={info.email} onChange={changedata}/>
+
  
 </form>
 </div>
