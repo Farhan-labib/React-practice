39624469a7dda7b57616c41ce0abec58ed58799a
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-18 10:52:26 +0600

    useeffect

--- myreact-app/src/USComponent.js
+++ myreact-app/src/USComponent.js
@@ -1,23 +1,12 @@
-import React,{useState} from 'react';
+import React,{useEffect} from 'react';
 
 export const USComponent=()=>{
     
-    const temp={
-        name:"FARHAN",
-        age:23,
-        Gender:"Male" };
-    const [info,changeinfo]= useState(temp);
-    const change=()=>{
-        changeinfo(
-            {
-                name:"LABIB"
-            }
-        )
-
-    };
+   useEffect(()=>{
+    alert("HELLO")
+   })
     return (
 <div>
-<h1>{info.name}</h1>
-<button onClick={change}>CLICK </button>
+
 </div>
 ) };
