e633f0b47abca9664575dc579fe71a453b851a0a
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-02-21 13:23:58 +0600

    routes and navbar

---
+++ myreact-app/src/Navbar.js
@@ -0,0 +1,12 @@
+import {Link } from 'react-router-dom';
+export const Navbar=()=>{
+    return (
+        <nav>
+        <Link to='/'>CHILD1</Link>
+        <Link to='/2'>CHILD2</Link>
+        <Link to='/3'>CHILD3</Link>
+ 
+ 
+        </nav>
+    )
+}
--- myreact-app/src/USComponent.js
+++ myreact-app/src/USComponent.js
@@ -1,16 +1,23 @@
 import React,{useRef} from 'react';
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes,Link } from 'react-router-dom';
 import {Child1} from './Child1';
 import {Child2} from './Child2';
 import {Child3} from './Child3';
+import {Navbar} from './Navbar';
+
 export const USComponent=()=>{
    
     return (
+        <>
+
+        <Navbar/>
         <Routes>
             <Route path='/' element={<Child1/>} />
             <Route path='/2' element={<Child2/>} />
             <Route path='/3' element={<Child3/>} />
         </Routes>
+        </>
+
 
     );
 };
